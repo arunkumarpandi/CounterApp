2682b8963136ecf5b248cb6137c810218b601c9e
Arunkumar <arun@Arunkumars-MacBook-Pro.local>
2022-01-28 16:36:44 +0530

    firebase setup completed

---
+++ lib/CustomDatabase.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomDatabase extends StatefulWidget {
+  const CustomDatabase({ Key? key }) : super(key: key);
+
+  @override
+  _CustomDatabaseState createState() => _CustomDatabaseState();
+}
+
+class _CustomDatabaseState extends State<CustomDatabase> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
